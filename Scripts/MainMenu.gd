803577a8625ed5f6ea46a7b965380f82766bdda8
Patch4Code <moritz-neumeier@outlook.de>
2023-12-23 16:26:34 +0100

    recap scene added

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,4 +4,4 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Recap.tscn")
---
+++ Scripts/Recap.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+func _on_button_1_pressed():
+	$verschluckt.visible = false
+	$Schiff.visible = true
+	
+func _on_button_2_pressed():
+	$Schiff.visible = false
+	$Papagei.visible = true
+
+func _on_button_3_pressed():
+	$Papagei.visible = false
+	$befreit.visible = true
+
+func _on_button_4_pressed():
+	$befreit.visible = false
+	$Karte.visible = true
+
+func _on_button_5_pressed():
+	$Karte.visible = false
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
