e7615a40f6fa82e0d1b0a2759cfec714493ca261
Patch4Code <moritz-neumeier@outlook.de>
2023-12-29 11:53:22 +0100

    working lightbeam with reflection

--- Scripts/Laser.gd
+++ Scripts/Laser.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const MAX_BOUNCES = 5
+
 @onready var ray = $RayCast2D
 @onready var line = $Line2D
 
@@ -9,8 +11,11 @@ func _process(delta):
 	line.add_point(Vector2.ZERO)
 	
 	ray.global_position = line.global_position
-	ray.target_position = ray.global_position + Vector2(1, 0) * 1000
+	ray.target_position = ((line.global_position+Vector2(1000,0))-line.global_position).normalized()*1000
 	ray.force_raycast_update()
+	
+	var prev = null
+	var bounces = 0
 	
 	while true:
 		if not ray.is_colliding():
@@ -30,12 +35,24 @@ func _process(delta):
 		if normal == Vector2.ZERO:
 			break
 		
+		#update collisions
+		if prev != null:
+			prev.collision_mask = 2
+			prev.collision_layer = 2
+		prev = coll
+		prev.collision_mask = 0
+		prev.collision_layer = 0
+		
+		#update raycast
 		ray.global_position = pt
 		ray.target_position = ray.target_position.bounce(normal)
 		ray.force_raycast_update()
-		
-		
-			
-			
-			
 	
+		#prevent infinite loop
+		bounces += 1
+		if bounces >= MAX_BOUNCES:
+			break
+	
+	if prev != null:
+		prev.collision_mask = 2
+		prev.collision_layer = 2
