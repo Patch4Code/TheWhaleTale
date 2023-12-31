301476a5d590b12c041e162f7e759f56d63f4f0d
Patch4Code <moritz-neumeier@outlook.de>
2023-12-31 14:15:59 +0100

    Boss Arena Design

--- Scripts/FogWall.gd
+++ Scripts/FogWall.gd
@@ -1,5 +1,9 @@
 extends StaticBody2D
 
+#Funktionen müssen noch angepasst werden, dass diese Später durch Signal aktiviert werden können
+# (bei einwerfen Münze Aufruf Inhalt make_fogwall_passable() und bei besiegen des Bosses Aufruf disable_fogwall()
+# Signale habe ich schon beim Laser in Kombination mit der door verwendet
+
 func make_fogwall_passable():
 	$CollisionShape2D.one_way_collision = true
 	
