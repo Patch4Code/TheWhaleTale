2cd43d2a40c7a015803a0b948e332c11908c12d9
enhuberfe85917 <enhuberfe85917@th-nuernberg.de>
2024-01-03 16:05:44 +0100

    Added first Version of Wall jump and moving Platforms

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 const SPEED = 450.0
 const JUMP_VELOCITY = -500.0
 
+const wall_jump_pushback = 100
+
+const wall_slide_gravity = 100
+var is_wall_sliding = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -19,6 +24,24 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
+	if is_on_wall() and Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = -wall_jump_pushback
+	if is_on_wall() and Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = wall_jump_pushback
+	if is_on_wall() and !is_on_floor():
+		if Input.is_action_pressed("ui_accept") or Input.is_action_pressed("ui_accept"):
+			is_wall_sliding = true
+		else:
+			is_wall_sliding = false
+	else:
+		is_wall_sliding = false
+	
+	if is_wall_sliding:
+		velocity.y += (wall_slide_gravity + delta)
+		velocity.y = min(velocity.y, wall_slide_gravity)
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
