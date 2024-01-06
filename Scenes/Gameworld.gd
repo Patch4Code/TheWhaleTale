bb58e34380c736e77d474bd26c2a0fc36ffb15ce
Patch4Code <moritz-neumeier@outlook.de>
2024-01-06 17:11:55 +0100

    spawn 2 + activation added

--- Scenes/Gameworld.gd
+++ Scenes/Gameworld.gd
@@ -9,6 +9,7 @@ var current_respawn
 
 func _ready():
 	current_respawn = start_respawn
+	$Checkpoint2.process_mode = Node.PROCESS_MODE_DISABLED
 
 func respawn_player():
 	$Player/DeathScreen.visible = true
@@ -16,6 +17,17 @@ func respawn_player():
 	$Player/DeathScreen.visible = false
 	emit_signal("player_revived")
 	$Player.position = current_respawn.global_position
+
+#activate respawn2 detection zone
+func _on_pirate_coin_coin_collected():
+	$Checkpoint2.process_mode = Node.PROCESS_MODE_INHERIT
+	
+#detection for activation of second spawn
+func _on_new_checkpoint_detection_body_entered(body):
+	if current_respawn != second_respawn:
+		if body.name == "Player":
+			print("respawn changed")
+			current_respawn = second_respawn
 	
 
 func _on_player_player_death():
@@ -24,7 +36,6 @@ func _on_player_player_death():
 func _process(delta):
 	if global.found_octootto_item == true:
 		$chest.visible = false
-
 
 
 
