5d797d1d938c852e52b29e5d735afad17937a6b5
Patch4Code <moritz-neumeier@outlook.de>
2024-01-06 17:33:44 +0100

    play boss music added

--- Scripts/FogOfWar.gd
+++ Scripts/FogOfWar.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
+		$"../../../BossMusic".playing = true
+		$"../../../MainGameMusic".playing = false
+		
+		
 		print("Fog of war entered")
 		$Area2D.monitoring = false
 		$".".visible = false
 
-
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		$"../../../BossMusic".playing = false
+		$"../../../MainGameMusic".playing = true
