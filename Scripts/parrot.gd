72622e2a6b4dc29f6bf2640bf542d8e546c5ead4
Patch4Code <moritz-neumeier@outlook.de>
2024-01-08 11:34:27 +0100

    parrot dialog added

--- Scripts/parrot.gd
+++ Scripts/parrot.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var parrot_before_boss_arena = false
 
+var player_in_range = false
+
 func _ready():
 	if parrot_before_boss_arena:
 		visible = false
@@ -11,3 +13,20 @@ func _on_pirate_coin_coin_collected():
 	#spawn parrot in front of arena
 	if parrot_before_boss_arena:
 		visible = true
+
+
+func _process(_delta):
+	if player_in_range:
+		if Input.is_action_just_pressed("Interact"):
+			DialogueManager.show_dialogue_balloon(load("res://parrot.dialogue"), "parrot")
+			return
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		player_in_range = true
+		$interact_view.visible = true
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		player_in_range = false
+		$interact_view.visible = false
--- global.gd
+++ global.gd
@@ -3,5 +3,7 @@ extends Node
 var found_octootto_item = false
 var given_octootto_item = false
 
+var parrot_dialog_open = false
+
 func change_to_ending_scene():
 	get_tree().change_scene_to_file("res://Scenes/Ending.tscn")
